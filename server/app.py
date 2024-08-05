df8ed98f2dd945cfffefb9955724770e98ad2786
Chris Lozzi <clozzi12@gmail.com>
2024-08-05 19:16:53 -0400

    cleanup

--- server/app.py
+++ server/app.py
@@ -1,11 +1,10 @@
-from flask import make_response, jsonify
 from config import app, api
 from flask_restful import Resource
 from models import User
 from sqlalchemy_serializer import SerializerMixin
 
 
-class Users(Resource, SerializerMixin):
+class Users(Resource):
     def get(self):
         users = [user.to_dict() for user in User.query.all()]
         print(users)
