69019d4a730e1cf2d622bdc33374422c149db859
Chris Lozzi <clozzi12@gmail.com>
2024-08-05 20:00:21 -0400

    proxy and test request of users to frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react"
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/users')
+    .then(r => r.json())
+    .then(data => {
+      setUsers(data)
+    })
+  }, [])
 
   return (
     <>
       <div>
         <p>Hello</p>
+        <label>Current Users: </label>
+        <ol>
+          {users.length > 0 ? (
+            users.map(user => (
+              <li key={user.id}>{user.name}</li>
+            ))
+          ) : (
+            <p>No Users</p>
+          )}
+        </ol>
       </div>
     </>
   )
--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
 from config import app, api
 from flask_restful import Resource
 from models import User
-from sqlalchemy_serializer import SerializerMixin
 
 
 class Users(Resource):
