0e4ca195b713be579482ab5613c849e212c34b49
Chris Lozzi <clozzi12@gmail.com>
2024-08-06 17:27:20 -0400

    quick flask auth views

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
-from config import app, api
-from flask import render_template
+from config import app, api, db
+from flask import render_template, request, session
 from flask_restful import Resource
 from models import User, Media
 
@@ -7,6 +7,60 @@ from models import User, Media
 @app.errorhandler(404)
 def not_found(e):
     return render_template("index.html")
+
+class Login(Resource):
+    def post(self):
+        name = request.get_json()['name']
+        password = request.get_json()['password']
+        user = User.query.filter_by(name=name).first()
+        if user:
+            if user.password == password:
+                session['user_id'] = user.id
+                return user.to_dict()
+            else:
+                return {'error': 'Incorrect password'}, 400
+        else:
+            return {'error': 'User not found'}, 400
+api.add_resource(Login, '/api/login')
+
+class Signup(Resource):
+    def post(self):
+        get_json = request.get_json()
+        name = get_json.get('name')
+        password = get_json.get('password')
+        user = User.query.filter_by(name=name).first()
+        if user:
+            return {'error': 'Username taken.'}, 400
+        else:
+            try:
+                new_user = User(
+                    name=name,
+                    password=password,
+                )
+                db.session.add(new_user)
+                db.session.commit()
+                session['user_id'] = new_user.id
+                return new_user, 200
+            except:
+                return {'error': 'Could not create user'}, 400
+api.add_resource(Signup, '/api/signup')
+
+class CheckSession(Resource):
+    def get(self):
+        id = session.get('user_id')
+        if id:
+            user = User.query.filter_by(id=id).first()
+            return user
+        else:
+            return {'error': 'Not Authorized'}, 401
+api.add_resource(CheckSession, '/api/check_session')
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {'message': 'Logged out'}, 204
+api.add_resource(Logout, '/api/logout')
+
 
 class Users(Resource):
     def get(self):
